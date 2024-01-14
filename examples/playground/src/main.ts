b7ab6abdecfe140fd95ae9324255f8742ac1b067
reona5 <shimada.reona@gmail.com>
2024-01-14 23:07:52 +0900

    初めてのレンダリングと createApp API
    refs: https://ubugeeei.github.io/chibivue/10-minimum-example/010-create-app-api.html

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,3 +1,9 @@
-import { helloChibivue } from "chibivue";
+import { createApp } from "chibivue";
 
-helloChibivue();
+const app = createApp({
+  render() {
+    return "Hello world.";
+  },
+});
+
+app.mount("#app");
---
+++ packages/runtime-core/renderer.ts
@@ -0,0 +1,9 @@
+export interface RendererOptions<HostNode = RendererNode> {
+  setElementText(node: HostNode, text: string): void;
+}
+
+export interface RendererNode {
+  [key: string]: any;
+}
+
+export interface RendererElement extends RendererNode {}
